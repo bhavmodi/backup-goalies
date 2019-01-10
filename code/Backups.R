ffb5c46243d0f1439968237b1f1656d615bd0a1b
bhavmodi <40410247+bhavmodi@users.noreply.github.com>
2019-01-09 20:08:23 -0500

    Update Backups.R

--- code/Backups.R
+++ code/Backups.R
@@ -5,7 +5,6 @@ library(lsr)
 ### For an explanation of the goalie stats, see:
 ### https://www.blueshirtsbreakaway.com/2017/11/16/a-beginners-guide-to-goaltender-advanced-stats/
 
-Goalies <- read.csv("C:\\Users\\Bhavik\\Google Drive\\Research\\Fun Shit\\Goalies\\Backups\\1417.csv")
 Goalies$PlayoffF <- factor(Goalies$Playoff,
                            level = c(0:1),
                            label = c("No", "Yes"))
@@ -18,7 +17,7 @@ table(Goalies$Starter)
 table(Backups$PlayoffF)
 
 ### Is there a diff in performance b/w backups on teams who made the playoffs and those who didn't?
-## What about adjusted save percentage?
+## Adjusted SV%
 aggregate(Backups$AdjStSVperc, by=list(Backups$PlayoffF), mean)
 aggregate(Backups$AdjStSVperc, by=list(Backups$PlayoffF), sd)
 leveneTest(AdjSVperc ~ PlayoffF, data = Backups) # homogeneity of variance
@@ -42,7 +41,7 @@ plot(Backups$PlayoffF, Backups$GSAA5v5)
 cohensD(GSAA5v5 ~ PlayoffF, data = Backups)
 # Pretty moderate effect size
 
-# dSV% is also pretty good
+# dSV% is also a pretty good measure
 aggregate(Backups$dSVperc, by=list(Backups$PlayoffF), mean)
 aggregate(Backups$dSVperc, by=list(Backups$PlayoffF), sd)
 leveneTest(dSVperc ~ PlayoffF, data = Backups) # homogeneity of variance
